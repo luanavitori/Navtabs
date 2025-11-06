52cb119e8137e4c971b890338efae1d712a11023
luanavitori <luanavha2007@gmail.com>
2025-11-05 20:13:41 -0400

    quasseee

--- App.tsx
+++ App.tsx
@@ -1,28 +1,38 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Feather } from "@expo/vector-icons";
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import { Feather, FontAwesome5, FontAwesome6 } from "@expo/vector-icons";
 
 import TelaInicio from "./src/components/TelaInicio";
 import TelaCatalogo from "./src/components/TelaCatalogo";
 import TelaContato from "./src/components/TelaContato";
 
-const Tabs = createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tabs.Navigator
+      <Tab.Navigator
         screenOptions={{
-          headerShown: false,
-          tabBarActiveTintColor: "blue",
-          tabBarInactiveTintColor: "gray",
+          headerStyle: {
+            backgroundColor: "#4A0000", 
+          },
+          headerTintColor: "#fff", 
+          headerTitleAlign: "center", 
+          headerTitleStyle: {
+            fontWeight: "bold",
+            fontSize: 18,
+          },
+          tabBarStyle: {
+            backgroundColor: "#4A0000",
+            borderTopColor: "#4A0000",
+          },
+          tabBarActiveTintColor: "#fff",
+          tabBarInactiveTintColor: "#C9AFAF",
         }}
       >
-        <Tabs.Screen
-          name="Inicio"
+        <Tab.Screen
+          name="Início"
           component={TelaInicio}
           options={{
             tabBarIcon: ({ color, size }) => (
@@ -31,103 +41,30 @@ export default function App() {
           }}
         />
 
-        <Tabs.Screen
+        <Tab.Screen
           name="Catálogo"
           component={TelaCatalogo}
           options={{
             tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="bottle-wine" size={24} color="black" />
-              
+              <FontAwesome5 name="wine-bottle" color={color} size={size} />
             ),
           }}
         />
 
-
-        <Tabs.Screen
+        <Tab.Screen
           name="Contato"
           component={TelaContato}
           options={{
             tabBarIcon: ({ color, size }) => (
-              <FontAwesome6 name="contact-book" size={24} color="black" />
-              
+              <FontAwesome6 name="address-book" color={color} size={size} />
             ),
           }}
         />
-      </Tabs.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from "react";
-// import { NavigationContainer } from "@react-navigation/native";
-// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-// import { Feather} from "@expo/vector-icons";
-
-// import TelaBiblioteca from "./src/components/TelaBiblioteca";
-// import TelaDescricao from "./src/components/TelaDescricao";
-// import TelaHome from "./src/components/TelaHome";
-// import TelaRotas from "./src/components/TelaHome";
-
-
-
-// const Tabs = createBottomTabNavigator();
-
-// export default function App () {
-//     return (
-//        <NavigationContainer>
-//             <Tabs.Navigator>
-//                 <Tabs.Screen name="Início" component = { TelaInicial} />
-//                 <Tabs.Screen name="Perfil" component = { TelaPerfil} />
-//             </Tabs.Navigator>
-//             <Tabs.Navigator screenOptions={{headerShown: false}}>
-//                  <Tabs.Screen name="Home" component = {TelaHome}
-//                 options={{
-//                     tabBarIcon:() =><Feather name="home" color={'blue'} size={30}/>,
-//                 }}
-//                 />
-//                  <Tabs.Screen name="Telas" component = {TelaDescricao}
-//                     options={{
-//                     tabBarIcon:() =><Feather name="monitor" color={'blue'} size={30}/>,
-//                 }}
-//                 />
-//                  <Tabs.Screen name="Rotas" component = {TelaRotas}
-//                     options={{
-//                     tabBarIcon:() =><Feather name="compass" color={'blue'} size={30}/>,
-//                 }}
-//                 />
-//                  <Tabs.Screen name="Biblioteca" component = {TelaBiblioteca}
-//                     options={{
-//                     tabBarIcon:() =><Feather name="book-open" color={'blue'} size={30}/>,
-//                 }}
-//                 />
-//             </Tabs.Navigator>
-//         </NavigationContainer>
-//     )
-// };
 
 
 
--- src/components/TelaContato/index.tsx
+++ src/components/TelaContato/index.tsx
@@ -1,13 +1,47 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Feather, FontAwesome5, FontAwesome } from "@expo/vector-icons";
 
-export default function TelaBiblioteca() {
+export default function TelaContato() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Contato</Text>
-      <Text style={styles.text}>
-       Entre em contato conosco para comprar nossos produtos
+      <Text style={styles.titulo}>
+        Entre em contato conosco para comprar nossos produtos
       </Text>
+
+      <View style={styles.card}>
+        <Feather name="phone" size={40} color="#800000" />
+        <View>
+          <Text style={styles.cardTitulo}>Telefone:</Text>
+          <Text style={styles.cardTexto}>+55 21 000000000</Text>
+        </View>
+      </View>
+
+      <View style={styles.card}>
+        <Feather name="map-pin" size={40} color="#800000" />
+        <View>
+          <Text style={styles.cardTitulo}>Endereço:</Text>
+          <Text style={styles.cardTexto}>
+            Av. 123, 222 - Rio de Janeiro RJ
+          </Text>
+        </View>
+      </View>
+
+      <View style={styles.card}>
+        <FontAwesome name="envelope" size={40} color="#800000" />
+        <View>
+          <Text style={styles.cardTitulo}>Email:</Text>
+          <Text style={styles.cardTexto}>preferida@adega.com.br</Text>
+        </View>
+      </View>
+
+      <View style={styles.card}>
+        <FontAwesome5 name="instagram" size={40} color="#800000" />
+        <View>
+          <Text style={styles.cardTitulo}>Instagram:</Text>
+          <Text style={styles.cardTexto}>@adegapreferida</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -15,16 +49,41 @@ export default function TelaBiblioteca() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
+    padding: 20,
     alignItems: "center",
-    justifyContent: "center",
   },
-  title: {
-    fontSize: 20,
+  titulo: {
+    fontSize: 24,
     fontWeight: "bold",
-    marginTop: 100
-  },
-  text: {
-    fontSize: 16,
     textAlign: "center",
+    color: "#000",
+    marginBottom: 30,
+    marginTop: 10,
+  },
+  card: {
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderWidth: 3,
+    borderColor: "#b9b4b4cb",
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 15,
+    width: "80%",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    gap: 12,
+  },
+  cardTitulo: {
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "#4A0000",
+    textAlign: "center"
+  },
+  cardTexto: {
+    fontSize: 16,
+    color: "#333",
   },
 });
